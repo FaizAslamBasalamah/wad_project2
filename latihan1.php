510ceed5b10b82848e2a6658b86f2c3292c95f14
FaizAslamBasalamah <115777566+FaizAslamBasalamah@users.noreply.github.com>
2023-10-19 14:51:52 +0700

    Add files via upload

---
+++ latihan1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan1</title>
+</head>
+<body>
+    <h4>Selamat Datang</h4>
+    <?php
+        echo "Faiz Aslam Basalamah\n";
+        $Angka1 = 5;
+        $Angka2 = 10;
+
+        echo $Angka1 + $Angka2;
+
+        while ($a <= 10) {
+            # code...
+        }
+        foreach ($variable as $key => $value) {
+            # code...
+        }
+        
+    ?>
+</body>
+</html>
